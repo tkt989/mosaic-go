09f68efdcd83b806d961dcd89ce2c75138170558
tkt989 <tkt989.dev@gmail.com>
2020-04-25 16:54:13 +0000

    update

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"image"
 	"image/color"
+	"image/jpeg"
 	_ "image/jpeg"
 	"image/png"
 	_ "image/png"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -20,36 +23,70 @@ func main() {
 		log.Fatal(err)
 	}
 
-	out, _ := os.Create("./out.png")
-	_ = png.Encode(out, mosaic(img, 13))
+	err = save(mosaic(img, 13), os.Args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func save(img image.Image, filename string) error {
+	ext := strings.ToLower(filepath.Ext(filename))
+	out, err := os.Create(filename)
+
+	switch ext {
+	case ".jpg":
+		err = jpeg.Encode(out, img, &jpeg.Options{})
+		break
+	case ".png":
+	default:
+		err = png.Encode(out, img)
+		break
+	}
+	return err
 }
 
 func mosaic(img image.Image, dot int) image.Image {
 	out := image.NewRGBA(img.Bounds())
+	width := img.Bounds().Max.X
+	height := img.Bounds().Max.Y
 
-	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y/dot; y++ {
-		for x := img.Bounds().Min.X; x < img.Bounds().Max.X/dot; x++ {
+	for y := img.Bounds().Min.Y; y <= height/dot; y++ {
+		for x := img.Bounds().Min.X; x <= width/dot; x++ {
 			px, py := x*dot, y*dot
+			num := 0
 
 			var r, g, b, a uint32
 			for dy := 0; dy < dot; dy++ {
 				for dx := 0; dx < dot; dx++ {
+					if width <= px+dx || height <= py+dy {
+						continue
+					}
+
 					pr, pg, pb, pa := img.At(px+dx, py+dy).RGBA()
 					r += pr
 					g += pg
 					b += pb
 					a += pa
+					num++
 				}
 			}
-			or := uint8(int(r) / (dot * dot))
-			og := uint8(int(g) / (dot * dot))
-			ob := uint8(int(b) / (dot * dot))
+			if num == 0 {
+				continue
+			}
 
-			oa := uint8(int(a) / (dot * dot))
+			or := uint16(int(r) / num)
+			og := uint16(int(g) / num)
+			ob := uint16(int(b) / num)
+
+			oa := uint16(int(a) / num)
 
 			for dy := 0; dy < dot; dy++ {
 				for dx := 0; dx < dot; dx++ {
-					out.Set(px+dx, py+dy, color.RGBA{R: or, G: og, B: ob, A: oa})
+					if width <= px+dx || height <= py+dy {
+						continue
+					}
+
+					out.Set(px+dx, py+dy, color.RGBA64{R: or, G: og, B: ob, A: oa})
 				}
 			}
 		}
